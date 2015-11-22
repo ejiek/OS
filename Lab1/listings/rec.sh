dce81e19629536ed520c938708b9feba358f2cb3
Vlad Petrov <ejiek@mail.ru>
2015-11-22 04:27:27 +0300

    rec.sh file output fixed

--- Lab1/listings/rec.sh
+++ Lab1/listings/rec.sh
@@ -12,7 +12,7 @@ fi
 while [ true  ] 
 do
 read -rp "$ " command
-eval "echo '$ $command' $append $output"
+eval printf \'%s\\n\' \"$command\" $append $output
 eval $command 2>&1 | tee -a $output
 append=">>"
 done
