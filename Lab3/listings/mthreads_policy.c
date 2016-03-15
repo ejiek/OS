0910eca731b7b5849cbe21dd4c563a635f743d31
Vlad Petrov <ejiek@mail.ru>
2016-03-15 11:31:17 +0300

    fixed: policies are more accurate now

--- Lab3/listings/mthreads_policy.c
+++ Lab3/listings/mthreads_policy.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-pthread_t t1,t2;
+pthread_t t1,t2, t3;
 struct sched_param sp;
 
 
@@ -40,51 +40,58 @@ void prnt_priority(int thread_number){
 
 
 void* thread1() {
-	sleep(1);
 	prnt_policy(1);
-	sleep(2);
+	sleep(3);
 	prnt_policy(1);
 }
 
 
 void* thread2() { 
-	sleep(1);
 	prnt_policy(2);
 	puts("Thread 2: change policy to SCHED_RR");
 	sp.sched_priority = sched_get_priority_min(SCHED_RR);
 	sched_setscheduler(0, SCHED_RR, &sp);
 	prnt_policy(2);
-	sleep(2);
-	puts("Thread 2: change policy to SCHED_OTHER by group id");
-	sp.sched_priority = sched_get_priority_min(SCHED_OTHER);
-	sched_setscheduler(getpid(), SCHED_OTHER, &sp);
-	prnt_policy(2);
 	
 }
 
 
+void* thread3() {
+	puts("Thread 3: change policy to SCHED_OTHER by group id");
+	sp.sched_priority = sched_get_priority_min(SCHED_OTHER);
+	sched_setscheduler(getpid(), SCHED_OTHER, &sp);
+	prnt_policy(3);
+}
 
 void main() {
 	prnt_policy(0);
 	puts("Main thread: create thread 1");
 	pthread_create(&t1, NULL, thread1, NULL);
+	sleep(1);
 
-	sleep(3);
 	puts("Main thread: switch policy to SCHED_FIFO");
 	sp.sched_priority = sched_get_priority_min(SCHED_FIFO);
 	sched_setscheduler(0, SCHED_FIFO, &sp);
 	prnt_policy(0);
-	
 	pthread_join(t1, NULL);
+	puts("");
+
 
 	puts("Main thread: create thread 2");
-	pthread_create(&t2, NULL, thread2, NULL);
-	sleep(2);
 	prnt_policy(0);
+	pthread_create(&t2, NULL, thread2, NULL);
+	pthread_join(t2, NULL);
+	prnt_policy(0);
+	puts("");
+
+
+	puts("Main thread: create thread 3");
+	prnt_policy(0);
+	pthread_create(&t3, NULL, thread3, NULL);
 	sleep(2);
 	prnt_policy(0);
 
-	pthread_join(t2, NULL);
+	pthread_join(t3, NULL);
 
 }
 
